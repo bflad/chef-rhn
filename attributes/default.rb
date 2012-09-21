e6a9616f244dcb95ace91361100a5adc786c1277
Brian Flad <bflad417@gmail.com>
2012-09-21 11:09:08 -0400

    FC001: Use strings in preference to symbols to access node attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,16 +18,16 @@
 #
 
 # At least one activation key required
-default[:rhn][:activation_keys] = ""
+default['rhn']['activation_keys'] = ""
 # rhn.redhat.com or RHN Satellite
-default[:rhn][:hostname] = "rhn.redhat.com"
+default['rhn']['hostname'] = "rhn.redhat.com"
 
-default[:rhn][:allow_config_actions]  = true
-default[:rhn][:allow_remote_commands] = true
-default[:rhn][:client_overrides]      = "client-config-overrides.txt"
-default[:rhn][:fully_update]          = true
-default[:rhn][:gpg]                   = true
-default[:rhn][:org_ca_cert]           = "rhn-org-trusted-ssl-cert-1.0-3.noarch.rpm"
-default[:rhn][:org_gpg_key]           = ""
-default[:rhn][:register]              = true
-default[:rhn][:ssl]                   = true
+default['rhn']['allow_config_actions']  = true
+default['rhn']['allow_remote_commands'] = true
+default['rhn']['client_overrides']      = "client-config-overrides.txt"
+default['rhn']['fully_update']          = true
+default['rhn']['gpg']                   = true
+default['rhn']['org_ca_cert']           = "rhn-org-trusted-ssl-cert-1.0-3.noarch.rpm"
+default['rhn']['org_gpg_key']           = ""
+default['rhn']['register']              = true
+default['rhn']['ssl']                   = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -24,8 +24,8 @@ template "/usr/local/src/rhn_setup/bootstrap.sh" do
   mode   "0755"
 end
 
-execute "RHN Bootstrapping to #{node[:rhn][:hostname]}" do
+execute "RHN Bootstrapping to #{node['rhn']['hostname']}" do
   cwd Chef::Config[:file_cache_path]
   command "/usr/local/src/rhn_setup/bootstrap.sh"
-  not_if "grep -q #{node[:hostname]} /etc/sysconfig/rhn/systemid"
+  not_if "grep -q #{node['hostname']} /etc/sysconfig/rhn/systemid"
 end
