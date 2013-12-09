5d91c1fc64f75218c279943eb8dcd79e03328444
Brian Flad <bflad417@gmail.com>
2013-12-08 21:45:44 -0500

    Rubocop automatic fixes

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer        "Brian Flad"
-maintainer_email  "bflad@wharton.upenn.edu"
-license           "Apache 2.0"
-description       "Registers node with RHN"
+maintainer        'Brian Flad'
+maintainer_email  'bflad@wharton.upenn.edu'
+license           'Apache 2.0'
+description       'Registers node with RHN'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.1"
-recipe            "rhn", "Registers node with RHN"
+version           '0.1.1'
+recipe            'rhn', 'Registers node with RHN'
 
-supports "redhat"
+supports 'redhat'
--- recipes/default.rb
+++ recipes/default.rb
@@ -18,25 +18,25 @@
 #
 
 if node['rhn']['activation_keys'].empty?
-  Chef::Log.error("RHN Activation Key missing, skipping RHN bootstrap.")
+  Chef::Log.error('RHN Activation Key missing, skipping RHN bootstrap.')
 else
-  directory "/usr/local/src/rhn_setup" do
-    owner "root"
-    group "root"
-    mode "0755"
+  directory '/usr/local/src/rhn_setup' do
+    owner 'root'
+    group 'root'
+    mode '0755'
     action :create
   end
 
-  template "/usr/local/src/rhn_setup/bootstrap.sh" do
-    source "bootstrap.sh.erb"
-    owner  "root"
-    group  "root"
-    mode   "0755"
+  template '/usr/local/src/rhn_setup/bootstrap.sh' do
+    source 'bootstrap.sh.erb'
+    owner  'root'
+    group  'root'
+    mode   '0755'
   end
 
   execute "RHN Bootstrapping to #{node['rhn']['hostname']}" do
     cwd Chef::Config[:file_cache_path]
-    command "/usr/local/src/rhn_setup/bootstrap.sh"
+    command '/usr/local/src/rhn_setup/bootstrap.sh'
     not_if "grep -q #{node['hostname']} /etc/sysconfig/rhn/systemid"
   end
 end
