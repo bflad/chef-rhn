fcd9f1d8f7d93b786ca2e4b71f48fc51fcae639a
Brian Flad <bflad417@gmail.com>
2013-12-08 22:41:02 -0500

    metadata fixes

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
+name              'rhn'
 maintainer        'Brian Flad'
-maintainer_email  'bflad@wharton.upenn.edu'
+maintainer_email  'bflad417@gmail.com'
 license           'Apache 2.0'
 description       'Registers node with RHN'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
