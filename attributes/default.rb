57fde39362decaa3671cc8e3e68c5e540d74ff8d
Brian Flad <bflad417@gmail.com>
2014-02-02 16:28:37 -0500

    Fix default attributes to support 0.2.0 enhancements

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,29 @@
-# At least one activation key required
-default['rhn']['activation_keys'] = ''
-# rhn.redhat.com or RHN Satellite
-default['rhn']['hostname'] = 'rhn.redhat.com'
+default['rhn']['activation_keys'] = nil
+default['rhn']['cmd_timeout'] = 300
+default['rhn']['hostname'] = 'xmlrpc.rhn.redhat.com'
+default['rhn']['password'] = 'changeme'
+default['rhn']['register'] = true
+default['rhn']['retries'] = 1
+default['rhn']['ssl'] = true
+default['rhn']['username'] = 'rhnuser'
 
-default['rhn']['allow_config_actions']  = true
-default['rhn']['allow_remote_commands'] = true
-default['rhn']['client_overrides']      = 'client-config-overrides.txt'
-default['rhn']['fully_update']          = true
-default['rhn']['gpg']                   = true
-default['rhn']['org_ca_cert']           = 'rhn-org-trusted-ssl-cert-1.0-3.noarch.rpm'
-default['rhn']['org_gpg_key']           = ''
-default['rhn']['register']              = true
-default['rhn']['ssl']                   = true
+default['rhn']['actions']['disabled'] = []
+default['rhn']['actions']['enabled'] = []
+
+default['rhn']['org_ca_cert']['action'] = 'install'
+default['rhn']['org_ca_cert']['name'] = 'RHNS-CA-CERT'
+default['rhn']['org_ca_cert']['url'] = "https://#{node['rhn']['hostname']}/pub/#{node['rhn']['org_ca_cert']['name']}"
+
+default['rhn']['org_gpg_key']['name'] = 'ORG-GPG-KEY'
+default['rhn']['org_gpg_key']['pub'] = nil
+default['rhn']['org_gpg_key']['url'] = nil
+
+default['rhn']['proxy']['password'] = nil
+default['rhn']['proxy']['url'] = nil
+default['rhn']['proxy']['username'] = nil
+
+default['rhn']['rhncfg-client']['server_url'] = 'https://%(server_name)s%(server_handler)s'
+default['rhn']['rhncfg-client']['script_log_file_enable'] = false
+
+default['rhn']['rhnsd']['enabled'] = false
+default['rhn']['rhnsd']['interval'] = 240
